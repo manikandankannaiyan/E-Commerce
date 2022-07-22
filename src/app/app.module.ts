027816d203eaa4a91ddf695507909c9beaf575d1
manikandan.siamgtp <manikandan.siamgtp@gmail.com>
2022-07-22 23:02:15 +0530

    home 30%(✔)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { WishlistComponent } from './pages/wishlist/wishlist.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,9 @@ import { HttpClientModule } from '@angular/common/http';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    SlickCarouselModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FakeApiService } from 'src/app/fake-api.service';
+import { FakeApiService } from 'src/app/service/fake-api.service';
+declare var $:any;
 
 @Component({
   selector: 'app-home',
@@ -7,13 +8,46 @@ import { FakeApiService } from 'src/app/fake-api.service';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  
+ apidata:any;
   constructor( private service:FakeApiService) { }
 
   ngOnInit(): void {
     this.service.getdata().subscribe((data:any)=>{
+      this.apidata=data
       console.log(data);
     })
+
+
+    $('.slider_for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider_nav'
+    });
+    $('.slider_nav').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      asNavFor: '.slider_for',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true
+    });
+        
   }
 
+  slideConfig = { 
+    slidesToShow: 2, 
+    slidesToScroll: 1,
+    arrows: false,
+    // dots:true,
+    vertical:true,
+    autoplay:true,
+    verticalSwiping:true
+  };
+
+
+
+  
 }
