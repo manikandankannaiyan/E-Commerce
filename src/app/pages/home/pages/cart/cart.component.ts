30f162f72723c99df142cea9d442ca16f49441df
manikandan.siamgtp <manikandan.siamgtp@gmail.com>
2022-07-28 17:45:07 +0530

    responsive completed

--- src/app/pages/home/pages/cart/cart.component.ts
+++ src/app/pages/home/pages/cart/cart.component.ts
@@ -10,43 +10,50 @@ export class CartComponent implements OnInit {
 
   n:any;
   cartlist:any;
-  totalprice:number=0;
   price:number=0;
   standardprice:any;
   notify:any;
+  totalamount:number=0;
+  amount:number=0;
   constructor(private api:fakeapidata) { }
 
   ngOnInit(): void {
     this.cartlist=this.api.carttemp;
     
     for(let item of this.cartlist){
-      this.totalprice=this.totalprice+item.price;
-
-      this.price=item.price;
-      this.n=item.addcount;
       this.standardprice=item.price/item.addcount;
       
+
+      this.totalamount=this.totalamount+item.price;
+      this.amount=this.totalamount;
     }
-    this.notify=this.cartlist.length;
+    this.notify=this.cartlist.length;    
     
   }
 
-  increament(){
+  increament(item:any){
     if (this.n==0 || this.n < 1) {
       this.n = 1;
     } 
-    this.n++;
-    this.price=this.standardprice*this.n;    
-    this.totalprice=this.price;
-   
+    if(item.addcount<item.stock){
+      let total=item.price/item.addcount;
+    
+      item.addcount++;
+      this.amount=total*item.addcount;
+      item.price=this.amount;
+     this.amount=this.totalamount-this.amount;
+    console.log(this.amount);
+    }
+    
   }
-  decreament(){
+  decreament(item:any){
     if (this.n ==0 || this.n < 1) {
       this.n = 1;
     }
-    this.n--;
-    this.price=this.standardprice*this.n;
-    this.totalprice=this.price;
+    item.addcount--;
+    item.price=this.standardprice*item.addcount;
+    this.amount=item.price
+    console.log(this.amount);
   }
 
   navbarCollapsed = true;
@@ -60,6 +67,6 @@ export class CartComponent implements OnInit {
   }
 
   checkout(){
-    alert('Total Amount:'+this.totalprice,)
+    alert('Total Amount:'+this.cartlist.price,)
   }
 }
